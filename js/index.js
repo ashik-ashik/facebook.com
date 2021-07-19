1ad3d6c8ec1ed2b1d9ea6ca283431cfbffb4b7ad
Md Ashik Ali <mdashika989@yahoo.con>
2021-07-20 00:05:53 +0600

    [UPLOAD] all files

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+
+  let selectA = $('#right-sidebar .contact-people > a');
+  $(selectA).hover(function(){
+    $('#quick-profile-view').addClass('hovered');
+    }, function(){
+    $('#quick-profile-view').removeClass('hovered');
+  });;
+
+console.log('ashik')
+
+});
